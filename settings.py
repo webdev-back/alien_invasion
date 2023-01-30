9a6f864e1aa64e890f11d14b8d95d80fa5f1bb34
John Smith <smith@mail.com>
2023-01-30 19:33:09 +0300

    Add bullet settings to Settings

--- settings.py
+++ settings.py
@@ -9,3 +9,8 @@ class Settings:
         self.caption = "Alien Invasion"
         # Ship settings
         self.ship_speed = 1.5
+        # Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
