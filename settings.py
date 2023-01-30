5cff55b17ae5a89ee1a70fa56d894bf1b12068fc
John Smith <smith@mail.com>
2023-01-30 21:25:38 +0300

    Craete alien element and display it on the screen

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ class Settings:
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (0, 0, 255)
+        self.bg_color = (230, 230, 230)
         self.caption = "Alien Invasion"
         # Ship settings
         self.ship_speed = 1.5
