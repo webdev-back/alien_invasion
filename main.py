fb3c694131ef4eb12a2bf890c9c920f2c33e65af
John Smith <smith@mail.com>
2023-01-31 23:06:38 +0300

    Create game over

--- main.py
+++ main.py
@@ -38,9 +38,12 @@ class AlienInvasion:
         while True:
             # Watch for keyboard and mouse events.
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+
+            if self.stats.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
+
             self._update_screen()
 
     def _update_bullets(self):
@@ -169,18 +172,22 @@ class AlienInvasion:
 
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
-        self.stats.ship_left -= 1
+        if self.stats.ship_left > 0:
+            # Decrement ships_left.
+            self.stats.ship_left -= 1
 
-        """Get rid of any remaining aliens and bullets."""
-        self.aliens.empty()
-        self.bullets.empty()
+            """Get rid of any remaining aliens and bullets."""
+            self.aliens.empty()
+            self.bullets.empty()
 
-        # Create a new fleet and center the ship.
-        self._create_fleet()
-        self.ship.center_ship()
+            # Create a new fleet and center the ship.
+            self._create_fleet()
+            self.ship.center_ship()
 
-        # Pause.
-        sleep(0.5)
+            # Pause.
+            sleep(0.5)
+        else:
+            self.stats.game_active = False
 
     def _check_aliens_bottom(self):
         """Check if any aliens have reached the bottom of the screen."""
