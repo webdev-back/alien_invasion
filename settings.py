e8cb41f361a05e387648df2e1af9267386fa4699
John Smith <smith@mail.com>
2023-02-01 18:02:58 +0300

    Add high score, level and quantity of ships per game

--- settings.py
+++ settings.py
@@ -1,27 +1,29 @@
 class Settings:
     """A class to store all settings for Alien Invasion."""
+
     def __init__(self):
         """Initialize the game's static settings."""
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
-        self.caption = "Alien Invasion"
 
         # Ship settings
         self.ship_limit = 3
 
         # Bullet settings
-        self.bullet_width = 5
+        self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
-        self.bullet_allowed = 3
+        self.bullets_allowed = 3
 
         # Alien settings
-        self.fleet_drop_speed = 5
+        self.fleet_drop_speed = 50
 
         # How quickly the game speeds up
         self.speedup_scale = 1.1
+        # How quickly the alien point values increase
+        self.score_scale = 1.5
 
         self.initialize_dynamic_settings()
 
@@ -29,7 +31,7 @@ class Settings:
         """Initialize settings that change throughout the game."""
         self.ship_speed = 1.5
         self.bullet_speed = 3.0
-        self.alien_speed = 1.0
+        self.alien_speed = 8.0
 
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
@@ -38,7 +40,9 @@ class Settings:
         self.alien_points = 50
 
     def increase_speed(self):
-        """Increase speed settings."""
+        """Increase speed settings and alien point values."""
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
--- ship.py
+++ ship.py
@@ -1,10 +1,14 @@
 import pygame
 
+from pygame.sprite import Sprite
 
-class Ship:
+
+class Ship(Sprite):
     """A class to manage the ship."""
+
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
+        super().__init__()
         self.screen = ai_game.screen
         self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
@@ -19,12 +23,12 @@ class Ship:
         # Store a decimal value for the ship's horizontal position.
         self.x = float(self.rect.x)
 
-        # Movement flag
+        # Movement flags
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
-        """Update the ship's position based on the movement flags."""
+        """Update the ship's position based on movement flags."""
         # Update the ship's x value, not the rect.
         if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
@@ -34,11 +38,11 @@ class Ship:
         # Update rect object from self.x.
         self.rect.x = self.x
 
+    def blitme(self):
+        """Draw the ship at its current location."""
+        self.screen.blit(self.image, self.rect)
+
     def center_ship(self):
         """Center the ship on the screen."""
         self.rect.midbottom = self.screen_rect.midbottom
         self.x = float(self.rect.x)
-
-    def blitme(self):
-        """Draw the ship at its current location."""
-        self.screen.blit(self.image, self.rect)
