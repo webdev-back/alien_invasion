d31f8253c408082a8b581b5577b415eab24eec79
John Smith <smith@mail.com>
2023-02-01 18:04:50 +0300

    Finish Alien Invasion Game

--- settings.py
+++ settings.py
@@ -18,7 +18,7 @@ class Settings:
         self.bullets_allowed = 3
 
         # Alien settings
-        self.fleet_drop_speed = 50
+        self.fleet_drop_speed = 10
 
         # How quickly the game speeds up
         self.speedup_scale = 1.1
@@ -31,7 +31,7 @@ class Settings:
         """Initialize settings that change throughout the game."""
         self.ship_speed = 1.5
         self.bullet_speed = 3.0
-        self.alien_speed = 8.0
+        self.alien_speed = 1.0
 
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
